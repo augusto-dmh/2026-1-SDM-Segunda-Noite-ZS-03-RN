72b2fed8de900134215acc5cdaf7e93a2b3b3e41
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-30 19:31:03 -0300

    adiciona cadastro de hospedes

--- navigation/DrawerNavigator.tsx
+++ navigation/DrawerNavigator.tsx
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CustomDrawerContent from './CustomDrawerContent';
 import HomeScreen from '../screens/HomeScreen';
+import CriarHospedeScreen from '../screens/hospedes/CriarHospedeScreen';
 import HospedesScreen from '../screens/hospedes/HospedesScreen';
 import CriarUsuarioScreen from '../screens/usuarios/CriarUsuarioScreen';
 import EditarUsuarioScreen from '../screens/usuarios/EditarUsuarioScreen';
@@ -23,6 +24,11 @@ export default function DrawerNavigator({ onLogout }: Props) {
       <Drawer.Screen name="Inicio" component={HomeScreen} />
       <Drawer.Screen name="Usuarios" component={UsuariosScreen} />
       <Drawer.Screen name="Hospedes" component={HospedesScreen} />
+      <Drawer.Screen
+        name="CriarHospede"
+        component={CriarHospedeScreen}
+        options={{ drawerItemStyle: { display: 'none' }, title: 'Criar hóspede' }}
+      />
       <Drawer.Screen
         name="CriarUsuario"
         component={CriarUsuarioScreen}
