703daae2abac16a12cff6a79576abf23ab1e2548
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-31 14:07:04 -0300

    adiciona cadastro de hospedes

---
+++ screens/hospedes/CriarHospedeScreen.tsx
@@ -0,0 +1,16 @@
+import TelaFormulario, {
+  CampoFormulario,
+} from '../../components/TelaFormulario';
+
+const campos: CampoFormulario[] = [
+  { nome: 'nome', label: 'Nome' },
+  { nome: 'email', label: 'E-mail', keyboardType: 'email-address' },
+  { nome: 'telefone', label: 'Telefone', keyboardType: 'phone-pad' },
+  { nome: 'documento', label: 'Documento' },
+  { nome: 'nacionalidade', label: 'Nacionalidade' },
+  { nome: 'data_nascimento', label: 'Data de nascimento' },
+];
+
+export default function CriarHospedeScreen() {
+  return <TelaFormulario endpoint="/hospedes/" campos={campos} />;
+}
