ddba10edbabba240c349de063be9cf8a8b9b58bd
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-31 18:38:20 -0300

    adiciona edicao de enderecos

---
+++ screens/enderecos/EditarEnderecoScreen.tsx
@@ -0,0 +1,17 @@
+import TelaFormulario, {
+  CampoFormulario,
+} from '../../components/TelaFormulario';
+
+const campos: CampoFormulario[] = [
+  { nome: 'logradouro', label: 'Logradouro' },
+  { nome: 'numero', label: 'Número' },
+  { nome: 'complemento', label: 'Complemento' },
+  { nome: 'bairro', label: 'Bairro' },
+  { nome: 'cidade', label: 'Cidade' },
+  { nome: 'estado', label: 'Estado' },
+  { nome: 'cep', label: 'CEP' },
+];
+
+export default function EditarEnderecoScreen() {
+  return <TelaFormulario endpoint="/enderecos/" campos={campos} />;
+}
