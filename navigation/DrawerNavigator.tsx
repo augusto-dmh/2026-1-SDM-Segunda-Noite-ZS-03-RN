b64ecc91cad2e09fb5f1e01257a7d1be4516fa9d
augusto-dmh <augustodemelohenriques@gmail.com>
2026-05-31 14:06:30 -0300

    adiciona edicao de usuarios

--- navigation/DrawerNavigator.tsx
+++ navigation/DrawerNavigator.tsx
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import CustomDrawerContent from './CustomDrawerContent';
 import HomeScreen from '../screens/HomeScreen';
 import CriarUsuarioScreen from '../screens/usuarios/CriarUsuarioScreen';
+import EditarUsuarioScreen from '../screens/usuarios/EditarUsuarioScreen';
 import UsuariosScreen from '../screens/usuarios/UsuariosScreen';
 
 const Drawer = createDrawerNavigator();
@@ -24,6 +25,11 @@ export default function DrawerNavigator({ onLogout }: Props) {
         name="CriarUsuario"
         component={CriarUsuarioScreen}
         options={{ drawerItemStyle: { display: 'none' }, title: 'Criar usuário' }}
+      />
+      <Drawer.Screen
+        name="EditarUsuario"
+        component={EditarUsuarioScreen}
+        options={{ drawerItemStyle: { display: 'none' }, title: 'Editar usuário' }}
       />
     </Drawer.Navigator>
   );
